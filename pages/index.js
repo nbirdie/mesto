d3102f004dd8c51679004781800dadf99e0ea512
Kate Shadrina <eolshadrina@mail.ru>
2022-06-08 21:09:41 +0300

    feature: popup 1

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
+const saveButton = document.querySelector(".popup__save-button");
+const popup = document.querySelector(".popup");
+const userName = document.querySelector(".profile__name");
+const userJob = document.querySelector(".profile__job");
+const popupForm = document.querySelector(".popup__form");
+
+const userNameInput = document.querySelector(".popup__field_type_name");
+const userJobInput = document.querySelector(".popup__field_type_job");
+
+function openPopup() {
+    popup.classList.remove('popup_view_hidden');
+    userNameInput.value = userName.textContent;
+    userJobInput.value = userJob.textContent;
+}
+
+function closePopup() {
+    popup.classList.add('popup_view_hidden');
+}
+
+
+editButton.addEventListener('click', function () {
+    openPopup();
+})
+
+closeButton.addEventListener('click', function () {
+    closePopup();
+})
+
+
