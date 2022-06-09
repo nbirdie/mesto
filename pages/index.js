d2df779b6b493eae2307c0292c3972b271a17623
Kate Shadrina <eolshadrina@mail.ru>
2022-06-09 12:58:40 +0300

    feature: popup js

--- pages/index.js
+++ pages/index.js
@@ -10,32 +10,31 @@ const userNameInput = document.querySelector(".popup__field_type_name");
 const userJobInput = document.querySelector(".popup__field_type_job");
 
 function openPopup() {
-    popup.classList.remove('popup_view_hidden');
-    userNameInput.value = userName.textContent;
-    userJobInput.value = userJob.textContent;
+  popup.classList.remove("popup_view_hidden");
+  userNameInput.value = userName.textContent;
+  userJobInput.value = userJob.textContent;
 }
 
 function closePopup() {
-    popup.classList.add('popup_view_hidden');
+  popup.classList.add("popup_view_hidden");
 }
 
 function formSubmitHandler(evt) {
-    console.log(1);
-    // evt.preventDefault();
-    // userNameInput.textContent = userName.value;
-    // userJobInput.textContent = userJob.value;
-    // closePopup();
-  }
-
+  evt.preventDefault();
+  userName.textContent = userNameInput.value;
+  userJob.textContent = userJobInput.value;
+  closePopup();
+}
 
 editButton.addEventListener("click", function () {
-    openPopup();
-})
+  openPopup();
+});
 
 closeButton.addEventListener("click", function () {
-    closePopup();
-})
+  closePopup();
+});
 
 saveButton.addEventListener("submit", function () {
-    formSubmitHandler ();
-})
+    console.log(2);
+    formSubmitHandler();
+});
