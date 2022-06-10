6bdc632a905b3c6572c0cf2362804f6857532923
Kate Shadrina <eolshadrina@mail.ru>
2022-06-10 23:05:02 +0300

    1st review

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ const userName = document.querySelector(".profile__name");
 const userJob = document.querySelector(".profile__job");
 const popupForm = document.querySelector(".popup__form");
 
+// для info редактирования
 const userNameInput = document.querySelector(".popup__field_type_name");
 const userJobInput = document.querySelector(".popup__field_type_job");
 
@@ -35,6 +36,16 @@ closeButton.addEventListener("click", function () {
 });
 
 saveButton.addEventListener("click", function (event) {
-    console.log(2);
-    formSubmitHandler(event);
+  formSubmitHandler(event);
 });
+
+// для лайка
+const elLikes = document.querySelectorAll(".elements__like");
+
+for (let i = 0; i < elLikes.length; i += 1) {
+  const elLike = elLikes[i];
+
+  elLike.addEventListener("click", function (event) {
+    event.target.classList.toggle("elements__like_active");
+  });
+}
